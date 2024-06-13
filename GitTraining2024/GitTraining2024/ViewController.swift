211e8a0a7659a0f2267a6dcd2bd7f191ef548671
Konst-Is <konst.is@mail.ru>
2024-06-13 12:57:01 +0300

    added project

---
+++ GitTraining2024/GitTraining2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTraining2024
+//
+//  Created by Константин on 13.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
